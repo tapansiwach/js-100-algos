211a7cba638a0317ac908db0d1df984aa5f116a2
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 11:46:38 -0500

    implement areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,24 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+  // find the stronger and weaker arms
+  let yourStronger, yourOther, friendsStronger, friendsOther;
+  if (yourLeft > yourRight) {
+    yourStronger = yourLeft;
+    yourOther = yourRight;
+  } else {
+    yourStronger = yourRight;
+    yourOther = yourLeft;
+  }
 
+  if (friendsLeft > friendsRight) {
+    friendsStronger = friendsLeft;
+    friendsOther = friendsRight;
+  } else {
+    friendsStronger = friendsRight;
+    friendsOther = friendsLeft;
+  }
+
+  // return true if strength of stronger and weaker arms are same
+  return yourStronger === friendsStronger && yourOther === friendsOther;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
