804ce2ac9901c9ad03c93b1aecb965a9b8d2bbad
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 10:18:24 -0500

    implement addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 function addTwoDigits(n: any): number {
-
+  // README mentions that this is expected to be a two digit number
+  // split the digits of the two digit number
+  const ones = n % 10;
+  const tens = (n - ones) / 10;
+  return ones + tens;
 }
 
 // console.log(addTwoDigits(29));
