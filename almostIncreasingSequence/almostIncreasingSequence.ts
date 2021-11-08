3cbf56b9ad931d383cbdc7917a2b9eff536fc353
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 11:00:00 -0500

    implement almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,24 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  let biggest = sequence[0];
+  let removedIndex;
+  // loop through the sequence
+  for (let index = 1; index < sequence.length; index++) {
+    const number = sequence[index];
+    // if number is smaller than already encountered
+    if (number < biggest) {
+      // try to remove an index if not already removed an index earlier
+      if (!removedIndex) {
+        removedIndex = index;
+      } else {
+        return false;
+      }
+    } else {
+      // otherwise, set this number as the biggest encountered so far
+      biggest = number;
+    }
+  }
 
+  return true;
 }
 
 // console.log(almostIncreasingSequence([1, 3, 2, 1])) 
