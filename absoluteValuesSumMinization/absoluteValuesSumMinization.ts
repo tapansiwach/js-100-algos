47a1c066b5327d78bd5f7040808e9ef5b1172c5b
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 09:38:51 -0500

    implement absoluteValuesSumMinization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,5 +1,10 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+  // effectively, we're finding the middle value in the array it seems
+  // step-1: find the length of the array
+  const length = a.length;
+  // find the middle value
+  const middle = (length % 2 === 0) ? length / 2 - 1 : Math.floor(length / 2);
+  return a[middle];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
