254d41b808d6dd1dfa45e9095b340a222b7ee183
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 11:32:44 -0500

    implement alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,7 @@
 function alternatingSums(a: number[]): number[] {
-
+  return a.reduce((acc, x, index) => {
+    return index % 2 === 0 ? [acc[0] + x, acc[1]] : [acc[0], acc[1] + x];
+  }, [0, 0]);
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
