c6e091181f4da8add0993766f8c7032dad9bc001
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 11:11:27 -0500

    implement alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,8 @@
 function alphabeticShift(inputString: string): string {
-
+  return inputString.split("").map(char => {
+    if (char === "z") return "a";
+    return String.fromCharCode(char.charCodeAt(0) + 1);
+  }).join("");
 }
 
 console.log(alphabeticShift('crazy'));
