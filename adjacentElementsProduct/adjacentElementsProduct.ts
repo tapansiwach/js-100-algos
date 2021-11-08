7aad716cb6d7058012dc333e6960983a805805fb
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 10:09:50 -0500

    implement adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+  let maxProduct = Number.NEGATIVE_INFINITY; // we'll update this as we traverse the array
+  // loop through the array starting from the 2nd element
+  for (let index = 1; index < inputArray.length; index++) {
+    const product = inputArray[index] * inputArray[index - 1];
+    if (product > maxProduct) {
+      maxProduct = product;
+    }
+  }
+  return maxProduct;
 }
 
 //console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
