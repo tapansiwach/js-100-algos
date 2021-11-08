27d3f94e393cb00220f28a9b9b63f97509835c9d
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-08 11:27:54 -0500

    implement alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,19 @@
 function alphabetSubsequence(s: string): boolean {
-
+  // find the ascii code for the first letter in sequence
+  let maxAscii = s.charCodeAt(0);
+  // loop over the remaining sequence
+  for (let index = 1; index < s.length; index++) {
+    // find the ascii char code for the letter in sequence
+    const ascii = s.charCodeAt(index);
+    // if ascii code is lower than already encountered, return false...
+    if (ascii <= maxAscii) {
+      return false;
+    } else {
+      // ... otherwise, update the maximum ascii code encountered
+      maxAscii = ascii;
+    }
+  }
+  return true;
 }
 
 console.log(alphabetSubsequence('zab'))
