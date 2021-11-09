c6a16e4943e2f300c7653676b79a9ce6eaac4226
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-09 14:44:01 -0500

    implement arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,14 @@
 function arrayChange(inputArray: number[]): number {
- 
+  let upMoves = 0;
+  for (let index = 1; index < inputArray.length; index++) {
+    const diff = inputArray[index] - inputArray[index - 1];
+    if (diff > 0) continue;
+
+    upMoves += 1 - diff;
+    inputArray[index] = inputArray[index - 1] + 1;
+  }
+
+  return upMoves;
 }
 
 console.log(arrayChange([1, 1, 1]));
