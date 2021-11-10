c9d6258c71675b1376b8a994284e491d69de10a6
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-10 17:18:38 -0500

    implement arrayConversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,24 @@
 function arrayConversion(inputArray: number[]): number {
+  let copy = [...inputArray];
+  let runCount = 0;
+  let output = [];
 
+  while (copy.length > 1) {
+    output.length = 0;
+    runCount += 1;
+    if (runCount % 2 === 1) {
+      for (let index = 0; index < copy.length - 1; index += 2) {
+        output.push(copy[index] + copy[index + 1]);
+      }
+    } else {
+      for (let index = 0; index < copy.length - 1; index += 2) {
+        output.push(copy[index] * copy[index + 1]);
+      }
+    }
+    copy = [...output];
+  }
+
+  return output[0];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
