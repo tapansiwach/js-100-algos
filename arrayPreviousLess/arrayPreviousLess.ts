2394e71e616bb02578b40737948b4667a426a949
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-11 06:27:46 -0500

    implement arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+  const copy = [...items];
+  const output = Array(items.length).fill(-1);
+  for (let index = 0; index < copy.length; index++) {
+    for (let idx = 0; idx < index; idx++) {
+      if (copy[idx] < copy[index]) {
+        output[index] = copy[idx];
+      }
+    }
+  }
+  return output;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
