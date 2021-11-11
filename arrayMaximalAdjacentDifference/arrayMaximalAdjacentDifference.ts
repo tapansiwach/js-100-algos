1ed3fe75d76f26dc8e6f94be8873edea463cfd48
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-11 06:14:18 -0500

    implement arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,10 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+  const diffs = [];
+  for (let index = 0; index < inputArray.length - 1; index++) {
+    diffs.push(inputArray[index] - inputArray[index + 1]);
+  }
+  diffs.map(x => Math.abs(x))
+  return Math.max(...diffs);
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
